71bd1e49870a292f0b49bad23b565508a3fade89
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-16 21:59:29 -0400

    working on navbar

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,25 +1,23 @@
-import AnchorLink from "react-anchor-link-smooth-scroll"
-
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
-    page: string;
-    selectedPage: string;
-    setSelectedPage: (value: string) => void;
-}
+  page: string;
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
+};
 
-function Link({page, selectedPage, setSelectedPage}: Props) {
-    const lowerCasePage = page.toLowerCase().replace(/ /g, "")
+function Link({ page, selectedPage, setSelectedPage }: Props) {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "");
   return (
-
-    <AnchorLink className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
-    transition duration-500 hover:text-primary-300
-    
-    `}
-    href={`#${lowerCasePage}`}
-     onClick={() => setSelectedPage(lowerCasePage)} >
-        {page}
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
+    transition duration-500 hover:text-primary-300`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
     </AnchorLink>
-  )
+  );
 }
 
-export default Link
+export default Link;
