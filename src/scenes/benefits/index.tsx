70afefa3307e8f4b2286c35042f0f336e9efa3a4
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-18 16:17:33 -0400

    benefits almost done

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,10 +1,42 @@
-import { SelectedPage } from "@/shared/types";
+import { BenefitType, SelectedPage } from "@/shared/types";
 import {
   HomeModernIcon,
   UserGroupIcon,
   AcademicCapIcon,
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
+import HText from "@/shared/HText";
+import Benefit from "./Benefit";
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className='h-6 w-6' />,
+    title: "State of the Art Facilities",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam luctus eros sit amet erat hendrerit, non ullamcorper tortor luctus. Praesent at est in mauris ornare sollicitudin.",
+  },
+  {
+    icon: <UserGroupIcon className='h-6 w-6' />,
+    title: "100's of Diverse Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam luctus eros sit amet erat hendrerit, non ullamcorper tortor luctus. Praesent at est in mauris ornare sollicitudin.",
+  },
+  {
+    icon: <HomeModernIcon className='h-6 w-6' />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam luctus eros sit amet erat hendrerit, non ullamcorper tortor luctus. Praesent at est in mauris ornare sollicitudin.",
+  },
+];
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -16,7 +48,44 @@ function Benefits({ setSelectedPage }: Props) {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        <div></div>
+        {/* Header */}
+        <motion.div
+          initial='hidden'
+          whileInView='visible'
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className='md:my-5 md:w-3/5'
+        >
+          <HText>MORE THAN JUST A GYM.</HText>
+          <p className='my-5 text-sm'>
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
+        </motion.div>
+
+        {/* Benefits */}
+        <motion.div
+          initial='hidden'
+          whileInView='visible'
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+          className='md:flex items-center justify-between gap-8 mt-5'
+        >
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </motion.div>
       </motion.div>
     </section>
   );
